9c7ed3bcaee11c0b34661cdc938597a616e69e41
emil joersboe <emil@randy>
2017-02-19 17:49:55 +0100

    cleaned up code a littlem put warning against putting ~ in path

--- R/plinkToRef.R
+++ R/plinkToRef.R
@@ -7,6 +7,11 @@ if(length(commandArgs(trailingOnly = T))>1){
     q() 
   }
 }
+
+if(grepl("~",plinkFile)){
+  print("Migt not work with '~' in path to plink files")
+}
+
 require(snpStats)
 
 plinkV2<-function(plinkFile){
@@ -22,8 +27,6 @@ plinkV2<-function(plinkFile){
   ind<-rownames(pl2)
   list(geno=pl2,bim=bim,fam=fam)
 }
-
-##plinkFile<-"/home/emil/OMNI5/CARDIO_METABOCHIP/datplus_QCed_newIDs_allcohorts_allSNPsJuli2015_nodups"
 
 plink<-plinkV2(plinkFile)
 
@@ -42,6 +45,8 @@ for(pop in unique(plink$fam$V1)){
 }
 f2<-do.call(cbind,l)
 
+## ref looks like this
+## id chr pos name A0_freq A1 French Han Karitiana Papuan Yoruba
 if(length(commandArgs(trailingOnly = T))>1){
   keep<-apply(f2[,1:ncol(f2)]>=maf,1,all) & apply(f2[,1:ncol(f2)]<=(1-maf),1,all)
   f3<-cbind(id=as.vector(paste(trimws(plink$bim$V1[keep]),trimws(plink$bim$V4[keep]),sep="_")),chr=plink$bim$V1[keep],pos=plink$bim$V4[keep],name=plink$bim$V2[keep],A0_freq=plink$bim$V5[keep],A1=plink$bim$V6[keep],f2[keep,])
@@ -49,19 +54,11 @@ if(length(commandArgs(trailingOnly = T))>1){
   f3<-cbind(id=as.vector(paste(trimws(plink$bim$V1),trimws(plink$bim$V4),sep="_")),chr=plink$bim$V1,pos=plink$bim$V4,name=plink$bim$V2,A0_freq=plink$bim$V5,A1=plink$bim$V6,f2)  
 }
 
-
-
 nInd<-rbind(names(table(plink$fam$V1)),as.vector(table(plink$fam$V1)))
-
-
-## ref looks like this
-## id chr pos name A0_freq A1 French Han Karitiana Papuan Yoruba
-
 
 name<-tail(unlist(strsplit(plinkFile,"/")),1)
 
 sites<-cbind(f3[,2:3],f3[,5:6])
-
 
 write.table(f3,paste("refPanel_",name,".txt",sep=""),col=T,row=F,quote=F)
 write.table(nInd,paste("nInd_",name,".txt",sep=""),col=F,row=F,quote=F)
